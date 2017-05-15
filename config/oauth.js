87c72ceee4dd5db83da3ef5f0ff36fc4a03f85ff
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-15 21:08:14 +0100

    small change to fb login link

--- config/oauth.js
+++ config/oauth.js
@@ -19,7 +19,7 @@ module.exports = {
     scope: 'user:email',
     appURL: process.env.APP_URL || 'http://localhost:3000/',
     getLoginURL() {
-      return `${this.loginURL}?client_id=${this.clientId}&redirect_uri=${this.appURL}oauth/facebook`;
+      return `${this.loginURL}?client_id=${this.clientId}&redirect_uri=${this.appURL}/oauth/facebook`;
     }
   }
 };
